6bb0f4555335b8accb76b252f5982f06ed87e214
rgcbrajesh <118043333+rgcbrajesh@users.noreply.github.com>
2024-01-09 05:21:40 +0000

    variable update

---
+++ basic/variable.js
@@ -0,0 +1,8 @@
+const accountName="brajesh";
+let accountNumber=411170002273;
+// var is not using in new javascript 
+var city="harda";
+city="indore"
+
+let acountState;
+console.table([accountName,accountNumber,city,acountState]);
