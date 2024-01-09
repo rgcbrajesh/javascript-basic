8c5adfe596eb90550da492934403fa2258e08708
rgcbrajesh <118043333+rgcbrajesh@users.noreply.github.com>
2024-01-09 05:54:00 +0000

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,15 @@
+let name="brajesh";  //string
+let age=19; //number
+let isLoggedIn=true; //boolean
+let state; //undefined
+let city=null //null
+
+console.table([name,age,isLoggedIn,state,city])
+
+
+// datattypes
+// string 
+// number 
+// boolean 
+// undefined  = undefined
+// null  = object
