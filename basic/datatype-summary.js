7293063d32514db89a4e2c8f38b30599418d4157
rgcbrajesh <118043333+rgcbrajesh@users.noreply.github.com>
2024-01-10 05:00:25 +0000

    datatype-summary

---
+++ basic/datatype-summary.js
@@ -0,0 +1,7 @@
+// DatattpeType
+
+// Primitive
+    // Type : String , Number , Boolean , Null , Undefined , Symbol , BigInt 
+
+//    Non Primitive (Reference) 
+    // Type : Array , Object , Function
